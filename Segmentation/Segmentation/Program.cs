8344255b8c475b83b03cd8064143a0d089bad605
KotoRina <kotmarvell@yandex.com>
2020-05-25 12:37:52 +0300

    [new]add assets image

---
+++ Segmentation/Segmentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+namespace Segmentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"C:\Selective_Search\Assets\input.jpg";
+
+            Bitmap input = new Bitmap(inputPath);
+
+
+        }
+    }
+}
