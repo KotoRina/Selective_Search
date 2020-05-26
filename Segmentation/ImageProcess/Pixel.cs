22439dfe36232e63579a0a2bc2d6f759e0e5ed99
KotoRina <kotmarvell@yandex.com>
2020-05-26 04:56:58 +0300

    [new]ImageProcess: add bitmap-to-matrix conversion

---
+++ Segmentation/ImageProcess/Pixel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageProcess
+{
+    public class Pixel
+    {
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+        public double A { get; set; }
+        public long X { get; set; }
+        public long Y { get; set; }
+    }
+}
---
+++ Segmentation/ImageProcess/PixelImagePresentation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace ImageProcess
+{
+    public class PixelImagePresentation
+    {
+        public Pixel[,] PixelMatrix { get; set; }
+        public string Path { get; set; }
+
+        public PixelImagePresentation()
+        {
+        }
+
+        public PixelImagePresentation( Bitmap image)
+        {
+            PixelMatrix = GetMatrixFromBitmap(image);
+        }
+
+        public PixelImagePresentation(string path)
+        {
+        }
+
+        public Pixel[,] GetMatrixFromBitmap(Bitmap image)
+        {
+            long height = image.Height;
+            long width = image.Width;
+
+            Pixel[,] result = new Pixel[height , width]; //матрица для записи результата
+            for(int i = 0; i < height; i++)
+            {
+                for(int l = 0; l < width; l++)
+                {
+                    Color colorPixel = image.GetPixel(i,l);
+                    Pixel pixel = new Pixel()
+                    {
+                        R = colorPixel.R,
+                        G = colorPixel.G,
+                        B = colorPixel.B,
+                        A = colorPixel.A,
+                        
+                        X = i,
+                        Y = l
+                    };
+
+                    result[i, l] = pixel;
+                }
+            }
+
+            return result;
+        }
+    }
+}
