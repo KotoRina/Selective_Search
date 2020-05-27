77911e0ab8640122802d3341379dbca4244aa6b8
KotoRina <kotmarvell@yandex.com>
2020-05-27 16:24:02 +0300

    [new] add class for binary image

---
+++ Segmentation/ContourAnalysis/BinaryImage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace ContourAnalysis
+{
+    public class BinaryImage
+    {
+        public Mat Image { get; set; }
+        public BinaryImage()
+        {
+        }
+
+        public BinaryImage(string path)
+        {
+            Mat matrix = new Mat(path);
+            Image = ConvetrImageToBinary(matrix);
+        }
+
+        public BinaryImage(Mat image)
+        {
+            Image = ConvetrImageToBinary(image);
+        }
+
+        public Mat ConvetrImageToBinary(Mat image)
+        {
+            Mat result = image;
+            
+            CvInvoke.CvtColor(result, result, ColorConversion.Bgr2Gray); //Преобразование в серый
+            CvInvoke.Threshold(result, result, 200, 255, ThresholdType.Binary); //Преобразовние в бинарное
+
+            return result;
+        }
+    }
+}
---
+++ Segmentation/ContourAnalysis/ContourSegmentation.cs
@@ -0,0 +1,23 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using System;
+
+namespace ContourAnalysis
+{
+    public class ContourSegmentation
+    {
+        public static VectorOfVectorOfPoint GetAllContours(Mat matrix)
+        {
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(matrix, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+
+            return contours;
+        }
+
+        public static void SortContours(VectorOfVectorOfPoint contours)
+        {
+
+        }
+    }
+}
