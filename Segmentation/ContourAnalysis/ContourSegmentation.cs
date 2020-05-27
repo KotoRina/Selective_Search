47ad5703e1238a7b9265568d1a13d6a36c1f58aa
KotoRina <kotmarvell@yandex.com>
2020-05-27 16:34:07 +0300

    [new]ContourSegmentation: add contour selection techniques

--- Segmentation/ContourAnalysis/ContourSegmentation.cs
+++ Segmentation/ContourAnalysis/ContourSegmentation.cs
@@ -1,7 +1,10 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace ContourAnalysis
 {
@@ -15,9 +18,35 @@ namespace ContourAnalysis
             return contours;
         }
 
-        public static void SortContours(VectorOfVectorOfPoint contours)
+        public static VectorOfVectorOfPoint SortContours(VectorOfVectorOfPoint contours)
         {
+            VectorOfVectorOfPoint contoursResult = new VectorOfVectorOfPoint();
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (contours[i].Size > 4)
+                {
+                    contoursResult.Push(contours[i]);
+                }
+            }
 
+            return contoursResult;
+        }
+
+        public static void GetRectangle(List<Rectangle> Box, VectorOfVectorOfPoint contours)
+        {
+            for(int i = 0; i < contours.Size; i++)
+            {
+                //TODO: сортировка по площади
+                Box.Add(CvInvoke.BoundingRectangle(contours[i]));
+            }
+        }
+
+        public static void DelineationOfRectangles(Mat matrix, List<Rectangle> Box, string pathSave = "")
+        {
+            foreach (var rectangle in Box)
+            {
+                CvInvoke.Rectangle(matrix, rectangle, new MCvScalar(0, 200, 0));
+            }
         }
     }
 }
